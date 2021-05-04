5865b22b4dd0fd13d7058a84318d6a064b0b6e7d
Sam Chris <thesamchris@gmail.com>
2021-05-04 11:07:14 +0800

    utilised links

--- App/screens/Options.js
+++ App/screens/Options.js
@@ -1,32 +1,38 @@
 import React from 'react';
+import { Alert, Linking, ScrollView } from 'react-native';
 import { Entypo } from '@expo/vector-icons';
 import SafeAreaView from '../components/SafeAreaView';
 import colors from '../constants/colors';
 import { RowItem, RowSeparator } from '../components/RowItem';
 
+const openUrl = url => {
+  return Linking.openURL(url).catch(() => Alert.alert('Sorry, something went wrong.', 'Pleasse try again later.'));
+};
 
 export default () => (
   <SafeAreaView>
-    <RowItem 
-      rightIcon={<Entypo name="chevron-right" size={20} color={colors.blue} />}
-      text="21 in 2021"
-      onPress={() => alert('yoooo!')}
-    />
+    <ScrollView>
+      <RowItem 
+        rightIcon={<Entypo name="chevron-right" size={20} color={colors.blue} />}
+        text="21 in 2021"
+        onPress={() => alert('yoooo!')}
+      />
 
-    <RowSeparator />
+      <RowSeparator />
 
-    <RowItem 
-      rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
-      text="Favoured Moments"
-      onPress={() => alert('yoooo!')}
-    />
+      <RowItem 
+        rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
+        text="Favoured Moments"
+        onPress={() => openUrl('https://www.instagram.com/favouredmoments.sg/')}
+      />
 
-    <RowSeparator />
+      <RowSeparator />
 
-    <RowItem 
-      rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
-      text="Visioglobe"
-      onPress={() => alert('yoooo!')}
-    />
+      <RowItem 
+        rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
+        text="Visioglobe"
+        onPress={() => openUrl('https://visioglobe.com/')}
+      />
+    </ScrollView>
   </SafeAreaView>
 );
