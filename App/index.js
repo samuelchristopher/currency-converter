4849e674ae1039cba1f856aa5aaed4ee8ad9ca09
Sam Chris <thesamchris@gmail.com>
2021-05-11 09:37:48 +0800

    added navigation

--- App/index.js
+++ App/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Home from './screens/Home';
+import Navigation from './config/Navigation';
 
-export default () => <Home />;
+export default () => <Navigation />;
 
 
